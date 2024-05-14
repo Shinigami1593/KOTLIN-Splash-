9ec5fa9f259cd118e0074eece4faedabff10221d
Ayush <rasaileeayush17@gmail.com>
2024-05-14 11:35:01 +0545

    lsanpjoeaivf[aowjnv[iuaeb

--- app/src/main/java/com/example/splashandbuttonnavigation/DashBoardActivity.kt
+++ app/src/main/java/com/example/splashandbuttonnavigation/DashBoardActivity.kt
@@ -6,13 +6,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.splashandbuttonnavigation.databinding.ActivityDashBoardBinding
 
 class DashBoardActivity : AppCompatActivity() {
     lateinit var binding: ActivityDashBoardBinding
 
-    private fun replaceFragment(){
-
+    private fun replaceFragment(fragment: Fragment){
+        var fragmentManager : FragmentManager = supportFragmentManager
+        var fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.display,fragment)
+        fragmentTransaction.commit()
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +28,13 @@ class DashBoardActivity : AppCompatActivity() {
 
         binding.bottomNagivation.setOnItemSelectedListener{
             when(it.itemId){
-                R.id.home -> replaceFragment()
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.search -> replaceFragment(SearchFragment())
+                R.id.notification -> replaceFragment(NotificationFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                else -> {}
             }
+            true
         }
 
 
