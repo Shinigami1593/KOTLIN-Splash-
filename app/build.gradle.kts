3bef8e6a582852c91c3b4b6a77faa3f9a7171673
Ayush <rasaileeayush17@gmail.com>
2024-05-14 11:20:41 +0545

    sujannnnnnnnnn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 android {
     namespace = "com.example.splashandbuttonnavigation"
     compileSdk = 34
+    buildFeatures{
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.example.splashandbuttonnavigation"
--- app/src/main/java/com/example/splashandbuttonnavigation/DashBoardActivity.kt
+++ app/src/main/java/com/example/splashandbuttonnavigation/DashBoardActivity.kt
@@ -1,16 +1,36 @@
 package com.example.splashandbuttonnavigation
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.splashandbuttonnavigation.databinding.ActivityDashBoardBinding
 
 class DashBoardActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDashBoardBinding
+
+    private fun replaceFragment(){
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dash_board)
+        binding = ActivityDashBoardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNagivation.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.home -> replaceFragment()
+            }
+        }
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
