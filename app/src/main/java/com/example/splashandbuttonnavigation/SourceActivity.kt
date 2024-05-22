aef687bd075c0396cc564eedb62f05397bab52fa
Ayush <rasaileeayush17@gmail.com>
2024-05-22 09:56:38 +0545

    haHAHAHAH

--- app/src/main/java/com/example/splashandbuttonnavigation/SourceActivity.kt
+++ app/src/main/java/com/example/splashandbuttonnavigation/SourceActivity.kt
@@ -1,16 +1,35 @@
 package com.example.splashandbuttonnavigation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.splashandbuttonnavigation.databinding.ActivitySourceBinding
 
 class SourceActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySourceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_source)
+        binding = ActivitySourceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            var intent = Intent(this@SourceActivity,
+                DestinationActivity::class.java)
+
+            var name: String = binding.newName.text.toString()
+            var phone: String = binding.newNumber.text.toString()
+
+            intent.putExtra("name", name)
+            intent.putExtra("phone", phone)
+            startActivity(intent)
+            finish()
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
